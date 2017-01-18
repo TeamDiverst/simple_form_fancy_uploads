545a8baa199b19021a25a8adadb8cc96fc6fd45b
Gabriel Givigier <gabriel@givigier.com>
2017-01-18 15:29:08 -0200

    Fix class of inputs

--- lib/simple_form_fancy_uploads/attachment_preview_input.rb
+++ lib/simple_form_fancy_uploads/attachment_preview_input.rb
@@ -4,7 +4,7 @@ module SimpleFormFancyUploads
       out = ''
       if object.send("#{attribute_name}?")
         out << template.link_to(object.send(attribute_name).original_filename,
-          object.send(attribute_name).expiring_url, class: "field__input-attachment-preview")
+          object.send(attribute_name).expiring_url, class: "field__input attach_preview")
       end
       (out << super).html_safe
     end
